a77a38622288f05636befc632482c0cb9e9c921c
NataliaSantosB <nataliasantos@MacBook-Air-de-Inventa.local>
2023-08-03 15:18:52 -0300

    add firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { initializeApp } from "firebase/app"; // Importe a função initializeApp
+import { getAnalytics } from "firebase/analytics"; // Importe a função getAnalytics
 import "./global.css";
 import Home from "./Pages/Home";
 import Details from "./Pages/Details";
 import Cast from "./Pages/Cast";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDozPqQIwQ6-hzIF6MHSgdafCRGlNAvOZA",
+  authDomain: "cinecast-cdbf5.firebaseapp.com",
+  databaseURL: "https://cinecast-cdbf5-default-rtdb.firebaseio.com",
+  projectId: "cinecast-cdbf5",
+  storageBucket: "cinecast-cdbf5.appspot.com",
+  messagingSenderId: "544321022134",
+  appId: "1:544321022134:web:d72b4089766a2a24c0de17",
+  measurementId: "G-4VQWKX80H3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <BrowserRouter>
